d549eff5d33778aec97060bc693b8946ca78f1c2
Adams <oadams97@yahoo.fr>
2021-05-28 00:12:43 +0200

    Add MVVM

--- app/src/main/java/com/example/news24/News.kt
+++ app/src/main/java/com/example/news24/News.kt
@@ -1,7 +1,7 @@
 package com.example.news24
 
 data class News(
-    val id: Int,
-    val title: String,
-    val description: String
+        val id: Int,
+        val title: String,
+        val description: String
 )
--- app/src/main/java/com/example/news24/presentation/list/NewsAdapter.kt
+++ app/src/main/java/com/example/news24/presentation/list/NewsAdapter.kt
@@ -7,7 +7,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.news24.News
 import com.example.news24.R
 import com.example.news24.R.layout.news_item
 
